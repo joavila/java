36e1c356bddb53f6f2b2404156bafe89f747acfe
Jorge Avila A <jorge.avila@oracle.com>
2022-08-23 13:34:38 -0400

    Improves formatting

--- tmp.I1JPAuziQF/HelloWorld.java
+++ tmp.I1JPAuziQF/HelloWorld.java
@@ -1,5 +1,7 @@
 import java.net.Socket;
 import java.util.concurrent.*;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
 import static java.util.concurrent.TimeUnit.*;
 
 class HelloWorld extends sun.net.NetworkClient {
@@ -11,23 +13,26 @@ class HelloWorld extends sun.net.NetworkClient {
 	protected ScheduledFuture<?> dox() {
 		final Runnable beeper = new Runnable() {
 			public void run() {
+				LocalDateTime localDateTime = LocalDateTime.now();
+				LocalTime localTime = localDateTime.toLocalTime();
 				try (Socket s = nc.doConnect("www.nowayjose1.com", 80)) {
-					if( s.isConnected()) {
-						System.out.println("OK");
+					if(s.isConnected()) {
+						System.out.format("%s - %s%n", localTime, "OK");
 					} else {
-						System.err.println("NOK");
+						System.err.format("%s - %s%n", localTime, "NOK");
 					}
 				} catch (Exception ex) {
-					System.err.println(ex);
+					System.err.format("%s - %s%n", localTime, ex);
 				}
 			}};
 		final ScheduledFuture<?> ret =
-			scheduler.scheduleAtFixedRate(beeper, 10, 10, SECONDS);
+			scheduler.scheduleAtFixedRate(beeper, 5, 3, SECONDS);
 		return ret;
 	}
 
 	public static void main( String ... args) throws Exception {
-		java.security.Security.setProperty("networkaddress.cache.ttl" , "600");
+		//java.security.Security.setProperty("networkaddress.cache.ttl" , "120");
+		//java.security.Security.setProperty("networkaddress.cache.negative.ttl" , "0");
 		nc.dox().get();
 	}
 }
